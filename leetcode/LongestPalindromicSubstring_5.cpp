a28d8344add2a7ea714da6e0b94c3bc6420cdcb7
haoyuzhang <gzzhanghaoyu@corp.netease.com>
2017-09-11 19:28:10 -0400

    leetcode 5

---
+++ leetcode/LongestPalindromicSubstring_5.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+	// if s[i] == s[j] then dp[i][j] = dp[i+1][j-1], here we optimize the space complexity to O(n).
+    string longestPalindrome(string s) {
+        int len = s.size();
+        if (!len) return "";
+        vector<bool> dp(len, false);
+        int start = 0, end = 0;
+        for(int i = len - 2; i >= 0; --i) {
+            for(int j = len - 1; j > i; --j) { // iterate reversely
+                if(s[i] == s[j]){
+                    if(j <= i + 2){ // naive answers.
+                        dp[j] = true;
+                    } else {
+                        dp[j] = dp[j - 1];
+                    }
+                    if(dp[j] && j - i > end - start) {
+                        start = i;
+                        end = j;
+                    }
+                } else {
+                    dp[j] = false;
+                }
+            }
+        }
+        return s.substr(start, end - start + 1);
+    }
+};
