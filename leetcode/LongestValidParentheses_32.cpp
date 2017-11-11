fc2218f481fc34557321809211fb904033a3cc51
haoyuzhang <zhangzhyzhy@gmail.com>
2017-11-10 22:36:38 -0500

    lc 32

---
+++ leetcode/LongestValidParentheses_32.cpp
@@ -0,0 +1,30 @@
+/*
+ * The idea here is to remove matched parentheses from the
+ * string, which leaves some "holes", and then find the 
+ * biggest "hole".
+ */
+
+typedef pair<int, bool> pr;
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        vector<pr> stk;
+        stk.push_back(pr(-1, false));
+        for(int i = 0; i < s.size(); ++i) {
+            if(s[i] == '(') {
+                stk.push_back(pr(i, true));
+            } else {
+                if(stk.back().second) {
+                    stk.pop_back();
+                } else {
+                    stk.push_back(pr(i, false));
+                }
+            }
+        }
+        int maxLen = 0;
+        stk.push_back(pr(s.size(), false));
+        for(int i = 0; i < stk.size() - 1; ++i)
+            maxLen = max(maxLen, stk[i + 1].first - stk[i].first - 1);
+        return maxLen;
+    }
+};
